cb91048bab3ce01763a4e0c802524878a3738e0e
Sumit Chouhan <sumit.chouhan@technoidentity.com>
2022-07-27 17:03:06 +0530

    Reducer-Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+// // import CounterOne from "./components/CounterOne";
+// // import CounterThree from "./components/CounterThree";
+// // import CounterTwo from "./components/CounterTwo";
+// import ComponentA from "./components/ComponentA";
+// import { useReducer, createContext } from "react";
+// import ComponentD from "./components/ComponentD";
+// import ComponentF from "./components/ComponentF";
+
+// export const CountContext = createContext();
+
+// const initialState = 0;
+
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case "increment":
+//       return state + 1;
+//     case "decrement":
+//       return state - 1;
+//     case "reset":
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
+
+// function App() {
+//   const [count, dispatch] = useReducer(reducer, initialState);
+//   return (
+//     <CountContext.Provider
+//       value={{ countState: count, countDispatch: dispatch }}
+//     >
+//       <div className="App">
+//         {/* <CounterOne />
+//       <CounterTwo />
+//       <CounterThree /> */}
+//         count - {count}
+//         <ComponentA />
+//         <ComponentD />
+//         <ComponentF />
+//       </div>
+//     </CountContext.Provider>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
+import DataFetchingOne from "./components/DataFetchingOne";
+import DataFetchingTwo from "./components/DataFetchingTwo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataFetchingOne />
+      <DataFetchingTwo />
     </div>
   );
 }
